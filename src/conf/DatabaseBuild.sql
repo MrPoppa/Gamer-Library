7699a07fb986e4eb21152dcc1b59d975d104b6f5
Benjamin Bengtsson <benjaminbenjibengtsson@gmail.com>
2015-10-25 11:44:05 +0100

    Created a sql file containing sql commands to create the database for this gamer library project.

---
+++ src/conf/DatabaseBuild.sql
@@ -0,0 +1,66 @@
+CREATE TABLE Owner(
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+firstName VARCHAR(64),
+lastName VARCHAR(64),
+ssn VARCHAR(12),
+userName VARCHAR(24),
+password VARCHAR(16)
+);
+
+CREATE TABLE Game_Genre (
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+genre VARCHAR(64)
+);
+
+CREATE TABLE Game_Rating (
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+rating VARCHAR(64)
+);
+
+CREATE TABLE Brand (
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+brandName VARCHAR(64)
+);
+
+CREATE TABLE Platform(
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+platformName VARCHAR(64),
+price INTEGER,
+buyDate DATE,
+brand_id INTEGER,
+CONSTRAINT fk_platform_brand FOREIGN KEY(brand_id) REFERENCES Brand(id)
+);
+
+CREATE TABLE Game (
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+gameName VARCHAR(64),
+price INTEGER,
+buyDate DATE,
+genre_id INTEGER,
+rating_id INTEGER,
+brand_id INTEGER,
+CONSTRAINT fk_game_genre FOREIGN KEY(genre_id) REFERENCES Game_Genre(id),
+CONSTRAINT fk_game_rating FOREIGN KEY(rating_id) REFERENCES Game_Rating(id),
+CONSTRAINT fk_game_brand FOREIGN KEY(brand_id) REFERENCES Brand(id)
+);
+
+CREATE TABLE Platform_Owner(
+owner_id INTEGER,
+platform_id INTEGER,
+CONSTRAINT fk_platform_owner FOREIGN KEY(owner_id) REFERENCES Owner(id),
+CONSTRAINT fk_owner_platform FOREIGN KEY(platform_id) REFERENCES Platform(id)
+);
+
+CREATE TABLE Game_Owner(
+owner_id INTEGER,
+game_id INTEGER,
+CONSTRAINT fk_game_owner FOREIGN KEY(owner_id) REFERENCES Owner(id),
+CONSTRAINT fk_owner_game FOREIGN KEY(game_id) REFERENCES Game(id)
+);
+
+CREATE TABLE Game_Platform(
+game_id INTEGER,
+platform_id INTEGER,
+CONSTRAINT fk_platform_game FOREIGN KEY(game_id) REFERENCES Game(id),
+CONSTRAINT fk_game_platform FOREIGN KEY(platform_id) REFERENCES Platform(id)
+);
