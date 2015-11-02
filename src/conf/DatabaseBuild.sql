9f0fd16414a6572847d79abf5ca890caeb40d96d
Benjamin Bengtsson <benjaminbenjibengtsson@gmail.com>
2015-11-02 14:15:54 +0100

    Modified database build and added some dummy data to be inserted.

--- src/conf/DatabaseBuild.sql
+++ src/conf/DatabaseBuild.sql
@@ -8,7 +8,7 @@ userName VARCHAR(24),
 password VARCHAR(16)
 );
 
-CREATE TABLE Game_Genre (
+CREATE TABLE Genre (
 id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 genre VARCHAR(64)
 );
@@ -18,7 +18,12 @@ id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 rating VARCHAR(64)
 );
 
-CREATE TABLE Brand (
+CREATE TABLE Platform_Brand (
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+brandName VARCHAR(64)
+);
+
+CREATE TABLE Game_Brand (
 id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 brandName VARCHAR(64)
 );
@@ -29,7 +34,7 @@ platformName VARCHAR(64),
 price INTEGER,
 buyDate DATE,
 brand_id INTEGER,
-CONSTRAINT fk_platform_brand FOREIGN KEY(brand_id) REFERENCES Brand(id)
+CONSTRAINT fk_platform_brand FOREIGN KEY(brand_id) REFERENCES Platform_Brand(id)
 );
 
 CREATE TABLE Game (
@@ -37,12 +42,12 @@ id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 gameName VARCHAR(64),
 price INTEGER,
 buyDate DATE,
-genre_id INTEGER,
+platform_id INTEGER,
 rating_id INTEGER,
 brand_id INTEGER,
-CONSTRAINT fk_game_genre FOREIGN KEY(genre_id) REFERENCES Game_Genre(id),
+CONSTRAINT fk_game_platform FOREIGN KEY(platform_id) REFERENCES Platform(id),
 CONSTRAINT fk_game_rating FOREIGN KEY(rating_id) REFERENCES Game_Rating(id),
-CONSTRAINT fk_game_brand FOREIGN KEY(brand_id) REFERENCES Brand(id)
+CONSTRAINT fk_game_brand FOREIGN KEY(brand_id) REFERENCES Game_Brand(id)
 );
 
 CREATE TABLE Platform_Owner(
@@ -59,9 +64,9 @@ CONSTRAINT fk_game_owner FOREIGN KEY(owner_id) REFERENCES Owner(id),
 CONSTRAINT fk_owner_game FOREIGN KEY(game_id) REFERENCES Game(id)
 );
 
-CREATE TABLE Game_Platform(
+CREATE TABLE Game_Genre(
 game_id INTEGER,
-platform_id INTEGER,
-CONSTRAINT fk_platform_game FOREIGN KEY(game_id) REFERENCES Game(id),
-CONSTRAINT fk_game_platform FOREIGN KEY(platform_id) REFERENCES Platform(id)
-);
+genre_id INTEGER,
+CONSTRAINT fk_genre_game FOREIGN KEY(game_id) REFERENCES Game(id),
+CONSTRAINT fk_game_genre FOREIGN KEY(genre_id) REFERENCES Genre(id)
+)
---
+++ src/conf/DummyData.sql
@@ -0,0 +1,116 @@
+--Platform brands
+INSERT INTO Platform_Brand (brandName)
+VALUES ('Sony');
+INSERT INTO Platform_Brand (brandName)
+VALUES ('Microsoft');
+INSERT INTO Platform_Brand (brandName)
+VALUES ('Nintendo');
+INSERT INTO Platform_Brand (brandName)
+VALUES ('Acer');
+INSERT INTO Platform_Brand (brandName)
+VALUES ('Lenovo');
+INSERT INTO Platform_Brand (brandName)
+VALUES ('Dell');
+INSERT INTO Platform_Brand (brandName)
+VALUES ('HP');
+INSERT INTO Platform_Brand (brandName)
+VALUES ('msi');
+INSERT INTO Platform_Brand (brandName)
+VALUES ('Samsung');
+INSERT INTO Platform_Brand (brandName)
+VALUES ('Asus');
+INSERT INTO Platform_Brand (brandName)
+VALUES ('Toshiba');
+INSERT INTO Platform_Brand (brandName)
+VALUES ('Apple');
+-- Game brands
+INSERT INTO Game_Brand (brandName)
+VALUES ('Bethesda Game Studios');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Interplay');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Naughty Dog');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Square Enix');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Atari');
+INSERT INTO Game_Brand (brandName)
+VALUES ('BioWare');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Black Isle Studios');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Blizzard Entertainment');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Bungie');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Capcom');
+INSERT INTO Game_Brand (brandName)
+VALUES ('EA');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Epic Games');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Infinity Ward');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Konami');
+INSERT INTO Game_Brand (brandName)
+VALUES ('LucasArts');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Maxis');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Ubisoft');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Valve');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Rockstar North');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Relic Entertainment');
+INSERT INTO Game_Brand (brandName)
+VALUES ('Namco');
+--Game genre
+INSERT INTO Genre (genre)
+VALUES ('RPG');
+INSERT INTO Genre (genre)
+VALUES ('Beat ''em up');
+INSERT INTO Genre (genre)
+VALUES ('FPS');
+INSERT INTO Genre (genre)
+VALUES ('Adventure');
+INSERT INTO Genre (genre)
+VALUES ('Platformer');
+INSERT INTO Genre (genre)
+VALUES ('Puzzle');
+INSERT INTO Genre (genre)
+VALUES ('Simulation');
+INSERT INTO Genre (genre)
+VALUES ('Strategy');
+INSERT INTO Genre (genre)
+VALUES ('Sports');
+INSERT INTO Genre (genre)
+VALUES ('Horror');
+INSERT INTO Genre (genre)
+VALUES ('Survival Horror');
+INSERT INTO Genre (genre)
+VALUES ('MMO');
+INSERT INTO Genre (genre)
+VALUES ('Shooter');
+INSERT INTO Genre (genre)
+VALUES ('Arcade');
+INSERT INTO Genre (genre)
+VALUES ('Fighting');
+INSERT INTO Genre (genre)
+VALUES ('Racing');
+--Game Rating
+INSERT INTO Game_Rating (rating)
+VALUES ('Early Childhood');
+INSERT INTO Game_Rating (rating)
+VALUES ('Everyone');
+INSERT INTO Game_Rating (rating)
+VALUES ('Everyone 10+');
+INSERT INTO Game_Rating (rating)
+VALUES ('Teen');
+INSERT INTO Game_Rating (rating)
+VALUES ('Mature');
+INSERT INTO Game_Rating (rating)
+VALUES ('Adults Only');
+INSERT INTO Game_Rating (rating)
+VALUES ('Raing Pending');
